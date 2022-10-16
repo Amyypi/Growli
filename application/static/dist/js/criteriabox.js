f35dd2f76a6fed6b6686ee5e06f8d5e65a88c6a3
Amyypi <minhthu_3@yahoo.com>
2022-10-16 11:33:36 -0400

    Technica 2022 Final code 2

---
+++ application/static/dist/js/criteriabox.js
@@ -0,0 +1,7 @@
+function data_function() {
+    return CreditScore + LTV + DTI + FEDTI
+}
+
+function userInfo_function(){
+    return ID + AppraisedValue + DownPayment + DownPayment + LoanAmount + MonthlyMortgagePayment
+}
